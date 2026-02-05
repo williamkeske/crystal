41600a5527a032ce2ab81df54e82027b8a2ee879
William Keske <wpkeske@gmail.com>
2026-02-05 08:20:46 -0300

    Custom

--- src/lua/functions/map/position_functions.cpp
+++ src/lua/functions/map/position_functions.cpp
@@ -193,7 +193,8 @@ int PositionFunctions::luaPositionSendMagicEffect(lua_State* L) {
 	}
 
 	MagicEffectClasses magicEffect = Lua::getNumber<MagicEffectClasses>(L, 2);
-	if (g_configManager().getBoolean(WARN_UNSAFE_SCRIPTS) && !g_game().isMagicEffectRegistered(magicEffect)) {
+	// if (g_configManager().getBoolean(WARN_UNSAFE_SCRIPTS) && !g_game().isMagicEffectRegistered(magicEffect)) {
+	if (magicEffect != MagicEffectClasses::CONST_ME_NONE && g_configManager().getBoolean(WARN_UNSAFE_SCRIPTS) && !g_game().isMagicEffectRegistered(magicEffect)) {
 		g_logger().warn("[PositionFunctions::luaPositionSendMagicEffect] An unregistered magic effect type with id '{}' was blocked to prevent client crash.", fmt::underlying(magicEffect));
 		Lua::pushBoolean(L, false);
 		return 1;
