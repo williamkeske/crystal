3ec4cd70310f16cc36cf17e717bd7c0bcf6539ec
William Keske <wpkeske@gmail.com>
2026-07-22 19:44:04 -0300

    Update weapons.cpp

--- src/items/weapons/weapons.cpp
+++ src/items/weapons/weapons.cpp
@@ -281,7 +281,7 @@ uint16_t Weapon::getWeaponAttackEffect(const std::shared_ptr<Item> &item, const 
 	}
 
 	// switch (item->getWeaponType()) {
-	switch (it.weaponType)
+	switch (it.weaponType) {
 		case WEAPON_SWORD:
 			return CONST_ME_SWORD_ATTACK;
 
